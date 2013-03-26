9d0ef0d55323a8fa629ea7a7c1dbd9fb0c91029e
Adrian Castillo <rodacato@gmail.com>
2013-03-26 15:47:03 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Crowd Interactive"
 maintainer_email  "adrian.castillo@crowdint.com"
 license           "MIT"
 description       "Configure frequently-used tools for an OSX workstation"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description  "Configure frequently-used tools for an OSX workstation"
 version           "1.0.0"
 supports          "mac_os_x"
 depends           "dmg"
