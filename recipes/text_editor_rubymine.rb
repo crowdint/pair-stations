e5169e592f446caa528be019c752b4a2397a1ca3
Adrian Castillo <rodacato@gmail.com>
2013-05-06 12:28:01 -0500

    Update text_editor_rubymine.rb

--- recipes/text_editor_rubymine.rb
+++ recipes/text_editor_rubymine.rb
@@ -1,6 +1,5 @@
 dmg_package "RubyMine" do
   source "http://download.jetbrains.com/ruby/RubyMine-5.0.2.dmg"
-  checksum "a10385a869d3b2bf99fbfa6f7b1206871e2a4a177aa6a4249fa8893cb523f0cb"
   action :install
   owner WS_USER
 end
